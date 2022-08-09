85f4f1896d7ef6bb78d86ebc1f12a9636c89f312
zeritte <zeritte@gmail.com>
2022-08-09 22:51:54 +0100

    add tests for main process

---
+++ test/index.test.ts
@@ -0,0 +1,84 @@
+const mock = require("mock-fs");
+import fs, { writeFileSync } from "fs";
+import { process } from "../index";
+
+jest.spyOn(fs, "writeFileSync");
+const mockWriteFileSync = writeFileSync as jest.MockedFunction<typeof writeFileSync>;
+
+describe("TEST main 'process' function", () => {
+  describe("WHEN the input file IS NOT found", () => {
+    beforeEach(() => {
+      mock({});
+    });
+
+    afterEach(() => {
+      mock.restore();
+      jest.clearAllMocks();
+    });
+
+    it("SHOULD return the no such file error", () => {
+      try {
+        process();
+      } catch (e) {
+        expect((e as Error).message).toContain("no such file or directory, open 'input.txt'");
+      }
+    });
+  });
+
+  describe("WHEN the input file IS found", () => {
+    describe("WHEN the input IS NOT properly structured", () => {
+      describe("WHEN the input DOES NOT have proper gird size", () => {
+        beforeEach(() => {
+          mock({
+            "input.txt": `salih salih`
+          });
+        });
+
+        afterEach(() => {
+          mock.restore();
+          jest.clearAllMocks();
+        });
+
+        it("SHOULD return invalid grid size error", () => {
+          try {
+            process();
+          } catch (e) {
+            expect((e as Error).message).toContain("Non-integer grid size provided");
+          }
+        });
+      });
+    });
+
+    describe("WHEN the input IS properly structured", () => {
+      beforeEach(() => {
+        mock({
+          "input.txt": `5 3
+1 1 E
+RFRFRFRF
+
+3 2 N
+FRRFLLFFRRFLL
+
+0 3 W
+LLFFFLFLFL`
+        });
+      });
+
+      afterEach(() => {
+        mock.restore();
+        jest.clearAllMocks();
+      });
+
+      it("SHOULD write the correct output to output.txt file", () => {
+        process();
+
+        expect(mockWriteFileSync).toHaveBeenCalledWith(
+          "output.txt",
+          `1 1 E
+3 3 N LOST
+2 3 S`
+        );
+      });
+    });
+  });
+});
