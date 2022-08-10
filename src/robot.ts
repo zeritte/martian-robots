e7772766e8081d22722defaa98c8ddceb30a1a83
zeritte <zeritte@gmail.com>
2022-08-10 10:52:07 +0100

    implement turn and move functions for robot

--- src/robot.ts
+++ src/robot.ts
@@ -35,6 +35,80 @@ class Robot {
     this.commands = commands;
   }
 
+  public move(
+    steps: number,
+    gridSize: [number, number],
+    lostScents: Array<[number, number]>
+  ): Array<[number, number]> {
+    const gridHorizontalSize = gridSize[0];
+    const gridVerticalSize = gridSize[1];
+
+    if (this.lost) {
+      return lostScents;
+    }
+
+    const canFall = !lostScents.some(
+      coordinates => coordinates[0] === this.x && coordinates[1] === this.y
+    );
+
+    let newX: number, newY: number;
+    switch (this.orientation) {
+      case Orientation.EAST:
+        newX = this.x + steps;
+        if (newX > gridHorizontalSize || newX < 0) {
+          if (!canFall) return lostScents;
+          this.lost = true;
+          break;
+        }
+        this.x = newX;
+        break;
+      case Orientation.WEST:
+        newX = this.x - steps;
+        if (newX > gridHorizontalSize || newX < 0) {
+          if (!canFall) return lostScents;
+          this.lost = true;
+          break;
+        }
+        this.x = newX;
+        break;
+      case Orientation.NORTH:
+        newY = this.y + steps;
+        if (newY > gridVerticalSize || newY < 0) {
+          if (!canFall) return lostScents;
+          this.lost = true;
+          break;
+        }
+        this.y = newY;
+        break;
+      case Orientation.SOUTH:
+        newY = this.y - steps;
+        if (newY > gridVerticalSize || newY < 0) {
+          if (!canFall) return lostScents;
+          this.lost = true;
+          break;
+        }
+        this.y = newY;
+        break;
+      default:
+        throw new Error("Unkown robot orientation state");
+    }
+
+    if (this.lost) {
+      return [...lostScents, [this.x, this.y]];
+    }
+    return lostScents;
+  }
+
+  public turn(clockwise: boolean) {
+    const currentOrientationIndex = SortedOrientations.findIndex(e => e === this.orientation);
+    // Getting the modulus of the new index, to keep it in the array boundaries
+    let newOrientationIndex =
+      ((clockwise ? currentOrientationIndex + 1 : currentOrientationIndex - 1) +
+        SortedOrientations.length) %
+      SortedOrientations.length;
+    const newOrientation = SortedOrientations[newOrientationIndex];
+    this.orientation = newOrientation;
+  }
 }
 
 export default Robot;
